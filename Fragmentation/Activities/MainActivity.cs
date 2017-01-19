521f43f825b6c99c77bda93f25773143b059cba5
Valerio Ferreira <valerio@outlook.com>
2017-01-18 22:21:40 -0200

    Initial project release. Android setup, Material Design and icons.

---
+++ Fragmentation/Activities/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Fragmentation.Activities
+{
+    [Activity(Label = "Fragmentation", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.main_activity);
+
+            var toolBar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolBar);
+        }
+    }
+}
