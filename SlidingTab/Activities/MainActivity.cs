22d87698eca1e4b3488db31058697ac31acdb8c4
Valerio Ferreira <valerio@outlook.com>
2017-01-31 00:46:16 -0200

    SlidingTab Start Project

---
+++ SlidingTab/Activities/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace SlidingTab.Activities
+{
+    [Activity(Label = "Sliding Tab", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.main_activity);
+
+            var toolBar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolBar);
+        }
+    }
+}
