7a753b1f975c93333da8c1fc8699ca81c62013db
Valerio Ferreira <valerio@outlook.com>
2017-02-03 22:59:10 -0200

    Add Monkey's data and images

---
+++ Monkeys/Data/Monkey.cs
@@ -0,0 +1,14 @@
+namespace Monkeys.Data
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        //URL for our monkey image!
+        public string Image { get; set; }
+
+        public string NameSort => Name[0].ToString();
+
+    }
+}
